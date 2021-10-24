37e8bf4606bc1873f777c1a1fd2a75428b3a1131
jamiep9rk <jamiecareer96@gmail.com>
2021-10-24 17:28:11 +0900

    add mypost page copying postlist page after merging sidebar animation

--- client/src/pages/mypost.style.js
+++ client/src/pages/mypost.style.js
@@ -1,3 +1,37 @@
 import styled from 'styled-components';
 
-export const MypageContainer = styled.div``;
+export const MypageContainer = styled.div`  
+display: flex;
+flex-wrap: wrap;
+margin-left: auto;
+margin-right: auto;
+margin-top: 40px;
+width: 70vw;
+height: 92%;
+`;
+export const ListmenuBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 30px;
+  border-bottom: solid 3px black;
+  margin-bottom: 4px;
+  font-size: 25px;
+`;
+export const ListdivBox = styled.div`
+  width: 100%;
+`;
+
+export const WritiBox = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  margin: 14px 0;
+`;
+
+export const PagenumBox = styled.div`
+  border: solid 1px orange;
+  width: 100%;
+  /* margin-top: 30px; */
+  text-align: center;
+`;
